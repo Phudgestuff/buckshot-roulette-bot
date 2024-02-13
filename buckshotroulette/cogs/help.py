5fc0dfd2e53f13af1f9310785ca9d0d49cda61e7
Phudgestuff <159431821+Phudgestuff@users.noreply.github.com>
2024-02-13 19:17:07 +0000

    Add files via upload

---
+++ buckshotroulette/cogs/help.py
@@ -0,0 +1,9 @@
+import discord
+from discord.ext import commands
+
+class help(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+def setup(client):
+    client.add_cog(help(client))
